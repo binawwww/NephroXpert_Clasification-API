63df0b1f43055cf1a51c5aacd834e86e8627ed25
binawwww <sabrinafebrianii17@gmail.com>
2026-04-20 19:37:53 +0700

    change the model for the new model

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import logging
 from datetime import datetime
 
 # Load model dan preprocessor
-model = joblib.load('fitur_SVM.pkl')
-preprocessor = joblib.load('preprocessor_fitur.pkl')
+model = joblib.load('svm_ckd_pipeline.pkl')
+preprocessor = joblib.load('selected_features .pkl')
 
 # Label klasifikasi (pastikan urutannya sesuai dengan label pada model)
 label_classes = ['Negatif CKD', 'Positif CKD']
@@ -46,20 +46,22 @@ def home():
                 'type': 'object',
                 'properties': {
                     'haemoglobin': {'type': 'number', 'example': 12.3},
+                    'packed_cell_volume': {'type': 'number', 'example': 40},
                     'specific_gravity': {'type': 'number', 'example': 1.02},
-                    'albumin': {'type': 'number', 'example': 0},
-                    'blood_glucose_random': {'type': 'number', 'example': 110},
-                    'sugar': {'type': 'number', 'example': 0},
-                    'potassium': {'type': 'number', 'example': 45},
-                    'blood_urea': {'type': 'number', 'example': 40},
-                    'blood_pressure': {'type': 'number', 'example': 80},
                     'serum_creatinine': {'type': 'number', 'example': 1.2},
-                    'sodium': {'type': 'number', 'example': 140}
+                    'appetite': {'type': 'string', 'example': 'good'},
+                    'hypertension': {'type': 'string', 'example': 'no'},
+                    'blood_urea': {'type': 'number', 'example': 40},
+                    'diabetes_mellitus': {'type': 'string', 'example': 'no'},
+                    'sodium': {'type': 'number', 'example': 140},
+                    'albumin': {'type': 'number', 'example': 0},
+                    'red_blood_cell_count': {'type': 'number', 'example': 5.0},
+                    'aanemia': {'type': 'string', 'example': 'no'}
                 },
                 'required': [
-                    'haemoglobin', 'specific_gravity', 'albumin', 'blood_glucose_random',
-                    'sugar', 'potassium', 'blood_urea', 'blood_pressure', 
-                    'serum_creatinine', 'sodium'
+                    'haemoglobin', 'packed_cell_volume', 'specific_gravity', 'serum_creatinine',
+                    'appetite', 'hypertension', 'blood_urea', 'diabetes_mellitus', 
+                    'sodium', 'albumin', 'red_blood_cell_count', 'aanemia'
                 ]
             }
         }
@@ -79,7 +81,8 @@ def home():
                             'Positif CKD': {'type': 'number', 'example': 0.8655}
                         }
                     },
-                    'timestamp': {'type': 'string', 'example': '2025-06-15T12:34:56.789123'}
+                    'timestamp': {'type': 'string', 'example': '2025-06-15T12:34:56.789123'},
+                    'note': {'type': 'string', 'example': 'Hasil ini merupakan prediksi berdasarkan model machine learning.Jika ingin memastikan keakuratan prediksi, segera periksa kefasilitas kesehatan (Faskes) terdekat untuk konsultasi dokter.'}
                 }
             }
         },
@@ -103,28 +106,25 @@ def predict():
         # Masukkan data ke DataFrame dengan urutan yang sesuai
         values = np.array([[
             float(data['haemoglobin']),
-            float(data['blood_glucose_random']),    
-            float(data['blood_urea']),
-            float(data['potassium']),
-            float(data['sodium']),
-            float(data['serum_creatinine']),
+            float(data['packed_cell_volume']),
             float(data['specific_gravity']),
-            float(data['blood_pressure']),
+            float(data['serum_creatinine']),
+            data['appetite'],
+            data['hypertension'],
+            float(data['blood_urea']),
+            data['diabetes_mellitus'],
+            float(data['sodium']),
             float(data['albumin']),
-            float(data['sugar'])
-
-
-
-
+            float(data['red_blood_cell_count']),
+            data['aanemia']
 
         ]]).reshape(1, -1)
 
-        features = ['haemoglobin', 'blood_glucose_random', 'blood_urea','potassium', 'sodium', 'serum_creatinine','specific_gravity', 'blood_pressure', 'albumin', 'sugar']
+        features = ['haemoglobin', 'packed_cell_volume', 'specific_gravity', 'serum_creatinine', 'appetite', 'hypertension', 'blood_urea', 'diabetes_mellitus', 'sodium', 'albumin', 'red_blood_cell_count', 'aanemia']
         
         input_df = pd.DataFrame(values, columns=features)
-        df_processed = preprocessor.transform(input_df)
-        predicted = model.predict(df_processed)[0]
-        probabilities = model.predict_proba(df_processed)[0]
+        predicted = model.predict(input_df)[0]
+        probabilities = model.predict_proba(input_df)[0]
         prob_dict = {
             label_classes[i]: float(probabilities[i]) for i in range(len(label_classes))
         }
@@ -134,7 +134,8 @@ def predict():
             "success": True,
             "predicted_label": label_classes[predicted],
             "probabilities": prob_dict,
-            "timestamp": datetime.now().isoformat()
+            "timestamp": datetime.now().isoformat(),
+            "note":"Hasil ini merupakan prediksi berdasarkan model machine learning.Jika ingin memastikan keakuratan prediksi, segera periksa kefasilitas kesehatan (Faskes) terdekat untuk konsultasi dokter."
         })
 
     except Exception as e:
